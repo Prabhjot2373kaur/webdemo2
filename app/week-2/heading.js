6f72ee1c5b30bcaa130763d88f140bdae3659412
Prabhjot2373kaur <152674371+Prabhjot2373kaur@users.noreply.github.com>
2024-09-11 12:55:47 -0600

    changes made

---
+++ app/week-2/heading.js
@@ -0,0 +1,7 @@
+export default function Heading(){
+    return(
+        <header>
+            <h1> week-2</h1>
+        </header>
+    )
+}
--- app/week-2/page.js
+++ app/week-2/page.js
@@ -1,8 +1,17 @@
-export default function page(){
+import Heading from "./heading";
+
+export default function Page(){
+
+    let a =5;
+    let b = 10;
+
     return(
         <main>
+            <Heading/>
             <h1>week 2</h1>
             <p>ther are 2 demos</p>
+            <p>the sum of a=b is(a+b).</p>
+           
         </main>
     );
 }
